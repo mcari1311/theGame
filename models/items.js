71d812b52e0bb39592a8658e8e3d3017cae528c0
Marie <mcari1311@gmail.com>
2022-08-08 11:54:12 -0400

    added items

--- models/items.js
+++ models/items.js
@@ -1,14 +1,45 @@
 const items = [
     {
-        name: 'rubix cude' ,
-        color: 'rainbow' ,
+        name: 'Keychain' ,
+        color: 'Pink' ,
+        cost: 5
+    },
+    {
+        name: 'Rubix Cube' ,
+        color: 'Rainbow' , 
+        cost: 10
+    }, 
+    {
+        name: 'Small Teddy Bear', 
+        color: 'Brown',
+        cost: 15
+    },
+    {
+        name: 'Mini Basketball',
+        color: 'Orange',
+        cost: 15
+    },
+    {
+        name: 'iPhone case',
+        color: 'Blue',
         cost: 20
     },
     {
-        name: 'keychain' ,
-        color: 'pink' , 
-        cost: 20
+        name: 'Board Game',
+        color: 'Multi',
+        cost: 50
+    },
+    {
+        name: 'Giant Teddy Bear',
+        color: 'Brown',
+        cost: 80
+    },
+    {
+        name: 'iPhone',
+        color: 'White',
+        cost: 300
     }
+
 ]
 
 module.exports = items
